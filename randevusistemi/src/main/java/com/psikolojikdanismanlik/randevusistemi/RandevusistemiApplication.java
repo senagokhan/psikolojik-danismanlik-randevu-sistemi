c019cc2cc698cb24a64546369424f355d47d39c8
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2025-04-24 13:18:35 +0300

    Proje-başlatıldı

---
+++ randevusistemi/src/main/java/com/psikolojikdanismanlik/randevusistemi/RandevusistemiApplication.java
@@ -0,0 +1,13 @@
+package com.psikolojikdanismanlik.randevusistemi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandevusistemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandevusistemiApplication.class, args);
+	}
+
+}
---
+++ randevusistemi/src/test/java/com/psikolojikdanismanlik/randevusistemi/RandevusistemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psikolojikdanismanlik.randevusistemi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandevusistemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
