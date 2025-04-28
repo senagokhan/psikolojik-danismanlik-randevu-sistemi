55878b03e7013d7ae7d763dada5968289f6ccf30
Sena Gökhan <160348164+senagokhan@users.noreply.github.com>
2025-04-28 11:49:49 +0300

    Availability entity sınıfı eklendi

---
+++ src/main/java/com/psikolojikdanismanlik/randevusistemi/entity/Availability.java
@@ -0,0 +1,44 @@
+package com.psikolojikdanismanlik.randevusistemi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "availabilities")
+public class Availability {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "therapist_id")
+    private Therapist therapist;
+
+    @Column(name = "start_time", nullable = false)
+    private LocalDateTime startTime;
+
+    @Column(name = "end_time", nullable = false)
+    private LocalDateTime endTime;
+
+    @Column(name = "available_date", nullable = false, updatable = false)
+    private LocalDate availableDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.availableDate = LocalDate.now();
+    }
+
+
+
+
+
+}
